15c4f88fc147ea3757b1bd842f69cc42f42f6ad4
DESKTOP-8H52H6S\yangeee <534650291@qq.com>
2019-09-14 18:53:57 +0800

    封装通用接口

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,31 +1,44 @@
 module.exports = app => {
   const express = require('express')
-  const router = express.Router()
-  const Category = require('../../model/Category')
-  app.use('/admin/api', router) //使用 express.Router类来让每一个请求前面自动加上这个路径
+  const router = express.Router({
+    mergeParams: true
+  })
+  app.use(
+    '/admin/api/rest/:resource',
+    async (req, res, next) => {
+      const modelName = require('inflection').classify(req.params.resource)
+      req.Model = require(`../../models/${modelName}`)
+      next()
+    },
+    router
+  ) //使用 express.Router类来让每一个请求前面自动加上这个路径
 
-  router.post('/categories', async (req, res) => {
-    const model = await Category.create(req.body) //调用数据库的表头create方法创建数据
+  router.post('/', async (req, res) => {
+    const model = await req.Model.create(req.body) //调用数据库的表头create方法创建数据
     res.send(model)
   })
-  router.put('/categories/:id', async (req, res) => {
-    const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+  router.put('/:id', async (req, res) => {
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
-  router.delete('/categories/:id', async (req, res) => {
-    await Category.findByIdAndDelete(req.params.id)
+  router.delete('/:id', async (req, res) => {
+    await req.Model.findByIdAndDelete(req.params.id)
     res.send({
       success: true
     })
   })
-  router.get('/categories', async (req, res) => {
-    const items = await Category.find()
-      .populate('parent')
+  router.get('/', async (req, res) => {
+    const queryOptions = {}
+    if (req.Model.modelName === 'Category') {
+      queryOptions.populate = 'parent'
+    }
+    const items = await req.Model.find()
+      .setOptions(queryOptions)
       .limit() //调用数据库的表头create方法创建数据
     res.send(items)
   })
-  router.get('/categories/:id', async (req, res) => {
-    const model = await Category.findById(req.params.id)
+  router.get('/:id', async (req, res) => {
+    const model = await req.Model.findById(req.params.id)
     res.send(model)
   })
 }
