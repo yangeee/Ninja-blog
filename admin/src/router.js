eae65c79222e286e0003f5316f10dcfecdc28162
yangeee <yangyi981121@gmail.com>
2020-02-18 00:48:42 +0800

    Complete the Login interface

--- admin/src/router.js
+++ admin/src/router.js
@@ -13,94 +13,42 @@ import AdEdit from './views/AdEdit.vue'
 import AdList from './views/AdList.vue'
 import AdminUserEdit from './views/AdminUserEdit.vue'
 import AdminUserList from './views/AdminUserList.vue'
+import Login from './views/Login.vue'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:'/login', name:"login",component: Login},
     {
       path: '/',
       name: 'main',
       component: Main,
       children: [
-        {
-          path: 'categories/create',
-          component: CategoryEdit
-        },
-        {
-          path: 'categories/list',
-          component: CategoryList
-        },
-        {
-          path: 'categories/edit/:id',
-          component: CategoryEdit,
-          props: true
-        },
-        {
-          path: 'items/create',
-          component: ItemEdit
-        },
-        {
-          path: 'items/list',
-          component: ItemList
-        },
-        {
-          path: 'items/edit/:id',
-          component: ItemEdit,
-          props: true
-        },
-        {
-          path: 'heroes/create',
-          component: HeroEdit
-        },
-        {
-          path: 'heroes/list',
-          component: HeroList
-        },
-        {
-          path: 'heroes/edit/:id',
-          component: HeroEdit,
-          props: true
-        },
-        {
-          path: 'articles/create',
-          component: ArticleEdit
-        },
-        {
-          path: 'articles/list',
-          component: ArticleList
-        },
-        {
-          path: 'articles/edit/:id',
-          component: ArticleEdit,
-          props: true
-        },
-        {
-          path: 'ads/create',
-          component: AdEdit
-        },
-        {
-          path: 'ads/list',
-          component: AdList
-        },
-        {
-          path: 'ads/edit/:id',
-          component: AdEdit,
-          props: true
-        },
-        {
-          path: 'admin_user/create',
-          component: AdminUserEdit
-        },
-        {
-          path: 'admin_user/list',
-          component: AdminUserList
-        },
-        {
-          path: 'admin_user/edit/:id',
-          component: AdminUserEdit,
-          props: true
-        }
+        { path: 'categories/create', component: CategoryEdit }, 
+        { path: 'categories/list', component: CategoryList },
+        { path: 'categories/edit/:id', component: CategoryEdit, props: true },
+        
+        { path: 'items/create', component: ItemEdit },
+        { path: 'items/list', component: ItemList },
+        { path: 'items/edit/:id', component: ItemEdit, props: true },
+        
+        { path: 'heroes/create', component: HeroEdit },
+        { path: 'heroes/list', component: HeroList },
+        { path: 'heroes/edit/:id', component: HeroEdit, props: true },
+        
+        { path: 'articles/create', component: ArticleEdit },
+        { path: 'articles/list', component: ArticleList },
+        { path: 'articles/edit/:id', component: ArticleEdit, props: true },
+        
+        { path: 'ads/create', component: AdEdit },
+        { path: 'ads/list', component: AdList },
+        { path: 'ads/edit/:id', component: AdEdit, props: true },
+        
+        { path: 'admin_user/create', component: AdminUserEdit },
+        { path: 'admin_user/list', component: AdminUserList },
+        { path: 'admin_user/edit/:id', component: AdminUserEdit, props: true }
       ]
     }
   ]
--- server/models/AdminUser.js
+++ server/models/AdminUser.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
   username: { type: String },
-  password: { 
-    type: String, 
-    select:false,
+  password: {
+    type: String,
+    select: false,
     set(val) {
       return require('bcrypt').hashSync(val, 10)
-  } }
+    }
+  }
 })
 
 module.exports = mongoose.model('AdminUser', schema) //命名一个表头，以及它存储的数据字段
