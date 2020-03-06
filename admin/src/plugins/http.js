a59e596b7e2cb7699a465b532bdc3ef524b9b4ba
yangeee <yangyi981121@gmail.com>
2020-03-06 16:21:52 +0800

    使用http-assert处理登录校验中的错误

--- admin/src/plugins/http.js
+++ admin/src/plugins/http.js
@@ -1,21 +1,39 @@
 import axios from 'axios'
 import Vue from 'vue'
+import router from "../router";
 
 const http = axios.create({
   baseURL: 'http://localhost:3000/admin/api'
 })
-
-//拦截响应，，根据message来提示用户错误的原因
+//拦截请求，加一个请求头附带token给后端
+http.interceptors.request.use(
+  config => {
+    if (localStorage.token) {
+      config.headers.Authorization = 'Bearer ' + localStorage.token
+    }
+    return config
+  },
+  err => {
+    return Promise.reject(err)
+  }
+)
+//拦截响应，根据message来提示用户错误的原因
 http.interceptors.response.use(
   res => {
     return res
   },
   err => {
-    Vue.prototype.$message({
-      type: 'error',
-      message: err.response.data.message
-    })
+    if(err.response.data.message){
+      Vue.prototype.$message({
+        type: 'error',
+        message: err.response.data.message
+      })
+    }
+    if(err.response.status === 401){
+      router.push('/login')
+    }
     return Promise.reject(err)
   }
+  
 )
 export default http
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,33 +1,57 @@
 module.exports = app => {
   const express = require('express')
+  const jwt = require('jsonwebtoken')
+  const AdminUser = require('../../models/AdminUser')
+  const assert = require('http-assert')
   const router = express.Router({
     mergeParams: true
   }) //router代表/admin/api/rest/:resource
 
+  // 创建资源
   router.post('/', async (req, res) => {
     const model = await req.Model.create(req.body) //调用数据库的表头create方法创建数据
     res.send(model)
   })
+  // 修改资源
   router.put('/:id', async (req, res) => {
     const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
+  // 删除资源
   router.delete('/:id', async (req, res) => {
     await req.Model.findByIdAndDelete(req.params.id)
     res.send({
       success: true
     })
   })
-  router.get('/', async (req, res) => {
-    const queryOptions = {}
-    if (req.Model.modelName === 'Category') {
-      queryOptions.populate = 'parent'
+  // 资源列表
+  router.get(
+    '/',
+    async (req, res, next) => {
+      //请求头后端要用小写开头，会自动对应
+      const token = String(req.headers.authorization || '')
+        .split(' ')
+        .pop()
+      assert(token, 401, '登录错误，请重新登录')
+      const { id } = jwt.verify(token, app.get('secret'))
+      assert(id, 401, '登录错误，请重新登录')
+      //必须挂载在req、res才能给下一个函数使用
+      req.user = await AdminUser.findById(id)
+      assert(req.user, 401, '请先登录')
+      await next()
+    },
+    async (req, res) => {
+      const queryOptions = {}
+      if (req.Model.modelName === 'Category') {
+        queryOptions.populate = 'parent'
+      }
+      const items = await req.Model.find()
+        .setOptions(queryOptions)
+        .limit() //调用数据库的表头create方法创建数据
+      res.send(items)
     }
-    const items = await req.Model.find()
-      .setOptions(queryOptions)
-      .limit() //调用数据库的表头create方法创建数据
-    res.send(items)
-  })
+  )
+  // 资源详情（已登录）
   router.get('/:id', async (req, res) => {
     const model = await req.Model.findById(req.params.id)
     res.send(model)
@@ -35,7 +59,9 @@ module.exports = app => {
 
   //图片上传接口
   const multer = require('multer')
-  const upload = multer({ dest: __dirname + '../../../uploads' })
+  const upload = multer({
+    dest: __dirname + '../../../uploads'
+  })
   app.post('/admin/api/upload', upload.single('file'), async (req, res) => {
     const file = req.file
     file.url = `http://localhost:3000/uploads/${file.filename}`
@@ -53,29 +79,35 @@ module.exports = app => {
     router
   ) //再使用 express.Router类来让每一个请求前面自动加上这个路径
 
+  // 登录接口
   app.post('/admin/api/login', async (req, res) => {
     const { username, password } = req.body
     //根据用户名找用户
-    const AdminUser = require('../../models/AdminUser')
-    const User = await AdminUser.findOne({ username }).select('+password')
-    if (!User) {
-      return res.status(422).send({
-        message: '用户不存在'
-      })
-    }
+    const User = await AdminUser.findOne({
+      username
+    }).select('+password')
+    assert(User, 422, '用户不存在')
     //校验密码
     const isValid = require('bcrypt').compareSync(password, User.password)
-    if (!isValid) {
-      return res.status(422).send({
-        message: '密码错误'
-      })
-    }
+    assert(isValid, 422, '密码错误')
     //返回token
-    const jwt = require('jsonwebtoken')
-    const token  = jwt.sign({ id: User._id}, app.get('secret'))
+    const token = jwt.sign(
+      {
+        id: User._id
+      },
+      app.get('secret')
+    )
     res.send({
       token,
       username: User.username
+    })
+  })
+
+  // 错误处理函数
+  app.use(async (err, req, res, next)=>{
+    console.log(err.status)
+    res.status(err.status || 500).send({
+      message: err.message
     })
   })
 }
